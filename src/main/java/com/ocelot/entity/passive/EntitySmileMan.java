1737869472fd07cea9f77a3a8714f3a62f2fc806
Ocelot5836 <FinntheRaider@gmail.com>
2017-11-10 09:14:51 -0800

    added basic smile man ai

--- src/main/java/com/ocelot/entity/passive/EntitySmileMan.java
+++ src/main/java/com/ocelot/entity/passive/EntitySmileMan.java
@@ -1,6 +1,14 @@
 package com.ocelot.entity.passive;
 
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.EntityAIAttackMelee;
+import net.minecraft.entity.ai.EntityAIHurtByTarget;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.ai.EntityAIWatchClosest;
+import net.minecraft.entity.monster.EntityEnderman;
 import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.entity.passive.EntityVillager;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.world.World;
 
 /**
@@ -10,5 +18,22 @@ public class EntitySmileMan extends EntityMob {
 
 	public EntitySmileMan(World world) {
 		super(world);
+        this.setSize(0.6f, 2);
+	}
+
+	@Override
+	protected void initEntityAI() {
+		tasks.addTask(10, new EntityAIWatchClosest(this, EntityVillager.class, 60));
+		tasks.addTask(100, new EntityAIWatchClosest(this, EntityPlayer.class, 60));
+		tasks.addTask(5, new EntityAIWander(this, 0.5));
+	}
+
+	@Override
+	protected void applyEntityAttributes() {
+		super.applyEntityAttributes();
+		this.getEntityAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(10.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(0.5);
+		this.getEntityAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(7.0D);
+		this.getEntityAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(64.0D);
 	}
 }
